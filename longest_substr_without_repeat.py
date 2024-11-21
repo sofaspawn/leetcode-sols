db97156ded2b6bb8ed352169d29fc6ddd59e5db0
sofaspawn <h.m1nus.work@gmail.com>
2024-11-21 13:04:17 +0530

    1st attempt

--- longest_substr_without_repeat.py
+++ longest_substr_without_repeat.py
@@ -1,2 +1,18 @@
 #! /usr/bin/env python3
-print("hello")
+
+def soln(s):
+    freq = {}
+    i = 0
+    lens = []
+    if len(s)==1:
+        return 1
+    while i<len(s):
+        if s[i] in freq:
+            lens.append(len(list(freq.keys())))
+            freq = {}
+        freq[s[i]]=1
+        i+=1
+    return max(lens)
+
+s = "pwwkew"
+print(soln(s))
