c9e1fc944ea399f0b6d91071b03c739edd2a8c59
sofaspawn <h.m1nus.work@gmail.com>
2024-11-23 22:07:48 +0530

    init

---
+++ rotating_the_box.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+class Solution:
+    def rotateTheBox(self, box):
+        pass
+
+box = [["#","#","*",".","*","."],
+       ["#","#","#","*",".","."],
+       ["#","#","#",".","#","."]]
+
+s = Solution()
+print(s.rotateTheBox(box))
