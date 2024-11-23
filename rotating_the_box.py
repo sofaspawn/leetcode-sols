a2ee5440c1745bf10008c3ee0680fd013016af69
sofaspawn <h.m1nus.work@gmail.com>
2024-11-23 22:54:04 +0530

    some progress

--- rotating_the_box.py
+++ rotating_the_box.py
@@ -1,8 +1,34 @@
-#! /usr/bin/env python3
+#! /usr/bin/env ipython3
 
 class Solution:
     def rotateTheBox(self, box):
-        pass
+        nbox = self.rotbox(box)
+        y = len(nbox)-1
+        while y>=0:
+            x = 0
+            while x<len(nbox[y]):
+                if y+1>=len(nbox):
+                    x+=1
+                    continue
+                else:
+                    if nbox[y+1][x]=="." and nbox[y][x]=="#":
+                        nbox[y+1][x] = nbox[y][x]
+                        nbox[y][x] = "."
+                    x+=1
+            y+=1
+        return nbox
+    def rotbox(self, box):
+        nbox = []
+        x = 0
+        while x<len(box[0]):
+            y = len(box)-1
+            row = []
+            while y>=0:
+                row.append(box[y][x])
+                y-=1
+            nbox.append(row)
+            x+=1
+        return nbox
 
 box = [["#","#","*",".","*","."],
        ["#","#","#","*",".","."],
@@ -10,3 +36,4 @@ box = [["#","#","*",".","*","."],
 
 s = Solution()
 print(s.rotateTheBox(box))
+#print(s.rotbox(box))
