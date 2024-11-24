eada3ae98978de29da5eeaa0089cf269741759fe
sofaspawn <h.m1nus.work@gmail.com>
2024-11-24 11:45:41 +0530

    init

---
+++ max_matrix_sum.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env ipython3
+
+class Solution:
+    def maxMatrixSum(self, matrix):
+        dirs = [[-1,0], [1,0], [0,1], [0,-1]]
+        for y in range(len(matrix)):
+            for x in range(len(matrix[y])):
+
+        return matrix
+
+matrix = [[1,2,3],[-1,-2,-3],[1,2,3]]
+
+s = Solution()
+print(s.rotateTheBox(box))
+#print(s.rotbox(box))
