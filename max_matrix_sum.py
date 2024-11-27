fcd5882be1545e3c6602d62a3a2ffe5df5080743
sofaspawn <h.m1nus.work@gmail.com>
2024-11-27 11:22:51 +0530

    add

--- max_matrix_sum.py
+++ max_matrix_sum.py
@@ -5,6 +5,10 @@ class Solution:
         dirs = [[-1,0], [1,0], [0,1], [0,-1]]
         for y in range(len(matrix)):
             for x in range(len(matrix[y])):
+                for dir in dirs:
+                    j, i = dir
+                    if y+j>=len(matrix) || y+j<0 || x+i>=len(matrix[y]) || x+i<0:
+                        continue
 
         return matrix
 
