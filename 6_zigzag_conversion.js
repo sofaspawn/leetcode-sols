3a3a9439a673072c1a5a68a2ddb600762a8b8a9f
sofaspawn <h.m1nus.work@gmail.com>
2025-01-05 13:27:41 +0530

    solved #6

--- 6_zigzag_conversion.js
+++ 6_zigzag_conversion.js
@@ -17,34 +17,72 @@
     * grid = [[""*numCols]*numRows]
     * x = 0
     * y = 0
+    *
+    * progression = "D"
+    *
     * for i in 0..s.len()
     *   grid[x][y] = s[i]
-    *   y += 1
-    *   if y == numRows
+    *   switch progression:
+    *       case "D":
+    *         y+=1
+    *         break
+    *       case "UDIA":
+    *         x+=1
+    *         y-=1
+    *         break
+    *       default:
+    *         break
+    *   if y==numRows
+    *     progression = "UDIA"
+    *   else if y==-1
+    *     progression = "D"
 */
 
 var convert = function(s, numRows) {
-    grid = Array(numRows).fill().map(() => Array(s.length).fill('_'));
+    grid = Array(numRows).fill().map(() => Array(s.length).fill(' '));
 
     let x = 0;
     let y = 0;
 
-    let progression = "D";
+    let downProgression = true;
 
     for (let i = 0; i<s.length; i++) {
-        grid[x][y] = s[i];
-        if (progression==="D"){
+        if (y >= numRows) {
+            x++;
+            y = numRows-2;
+            downProgression = false;
+        } else if (y < 0) {
+            downProgression = true;
+            y = 1;
+            x-=1;
+        }
+
+        grid[y][x] = s[i];
+
+        if(downProgression){
             y+=1;
+        } else {
+            x+=1;
+            y-=1;
         }
     }
 
-    renderGrid(grid);
+    let result = "";
+    for (let i = 0; i < grid.length; i++) {
+        for (let j=0; j < grid[i].length; j++) {
+            if (grid[i][j] != ' ') {
+                result += grid[i][j];
+            }
+        }
+    }
+    return result;
 };
 
 var renderGrid = function(grid) {
     for (let i = 0; i < grid.length; i++) {
         for (let j=0; j < grid[i].length; j++) {
             process.stdout.write(grid[i][j]);
+            process.stdout.write(" ");
         }
         console.log("");
     }
