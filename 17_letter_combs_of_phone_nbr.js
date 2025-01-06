4fe66ceae26b1785b84225c27a95abe57ddba6c5
sofaspawn <h.m1nus.work@gmail.com>
2025-01-06 09:35:49 +0530

    problem #17 init

---
+++ 17_letter_combs_of_phone_nbr.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (digits.length === 0) return [];
+    let phone = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    };
+    let result = [];
+    return result;
+};
+
+console.log(letterCombinations("23")); // ["ad","ae","af","bd","be","bf","cd","ce","cf"]
